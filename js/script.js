81339214ffa95259e52006e08f45a1a197281f20
emytek <66952283+emytek@users.noreply.github.com>
2022-08-24 01:25:32 +0100

    starter portfolio with JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developer", "Software Engineer", "Data Analyst", "Geoinformation Analyst"],
+    typeSpeed: 100,
+    Backspeed: 60,
+    loop:true
+})
